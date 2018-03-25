0915b0579c5fb7deeffaa2da2b8a474854a84f7c
Marco Ricupero <marco.ricupero@accenture.com>
2018-03-25 06:09:17 +0200

    + more verbose log + changed default root resorce to /mockettaro

--- commandline.js
+++ commandline.js
@@ -8,7 +8,7 @@ program
   .version(package.version)
   .description(package.description)
   .option('-p, --port <portNumber>', 'Serve on specified port', v=>parseInt(v), 8080)
-  .option('-r, --resource <resource>', 'Root resource to serve', /^[a-z0-9\_\-]*$/i, 'mock')
+  .option('-r, --resource <resource>', 'Root resource to serve', /^[a-z0-9\_\-]*$/i, 'mockettaro')
   .option('-d, --response-delay <responseDelay>', 'Response delay in ms', v=>parseInt(v), 0)
   .option('-t, --cache-lifetime <cacheLifetime>', 'JSON cache lifetime', v=>parseInt(v), 3000)
   .parse(process.argv);
@@ -19,5 +19,5 @@ const server = require('express')();
 server.use(`/${program.resource}`, mockettaro.serve('./', program));
 
 server.listen(program.port, ()=>{
-    console.log(`Mockettaro serveing on port ${program.port}`.magenta);
+  console.log(`Mockettaro serving ${process.cwd()} content @ localhost:${program.port}/${program.resource}`.magenta);
 });
