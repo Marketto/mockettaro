88987837af271e855b95031d04a94a0497a29d10
Marco Ricupero <marco.ricupero@gmail.com>
2018-05-12 17:11:05 +0200

    Implemented tests + support for default.METHOD.json & default.METHOD.code

---
+++ test/test.js
@@ -0,0 +1,64 @@
+const mockettaro = require('../');
+const server = require('express')();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('mockettaro', ()=>{
+    let testServer;
+    before(function (done) {
+        server.use(`/test`, mockettaro.serve('/examples/mocks'));
+        testServer = server.listen(9999, done);
+    })
+    describe('Cities Toulouse DELETE resource', ()=>{
+        it('Should return status 204 and an empty body', done => {
+            chai.request(server).delete('/test/cities/Toulouse')
+                .end((req, res)=>{
+                    res.status.should.be.equal(204);
+                    res.body.should.be.an('object').that.is.empty;
+                    done();
+                });
+        });
+    });
+
+    describe('Cities default PUT resource', () => {
+        it('Should return 202 and a body', done => {
+            chai.request(server).put('/test/cities/Rome')
+                .end((req, res) => {
+                    res.status.should.be.equal(202);
+                    res.body.should.be.an('object').that.have.property('status').that.is.equal("OK");
+                    done();
+                });
+        });
+    });
+
+    describe('Cities GET resource', ()=>{
+        it('Should return status 200 and an Array of cities on GET', done => {
+            chai.request(server).get('/test/cities')
+                .end((req, res) => {
+                    res.status.should.be.equal(200);
+                    res.body.should.be.an('array').that.is.not.empty;
+                    done();
+                });
+        });
+    });
+    describe('Cities POST resource', () => {
+        it('Should return 201 and a body', done => {
+            chai.request(server).post('/test/cities')
+                .end((req, res) => {
+                    res.status.should.be.equal(201);
+                    res.body.should.be.an('object').that.have.property('status').that.is.equal("OK");
+                    done();
+                });
+        });
+    });
+
+    after(() => {
+        if (testServer){
+            testServer.close();
+            testServer = undefined;
+        }
+    });
+});
