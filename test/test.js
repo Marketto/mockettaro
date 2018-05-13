624a61be3fd02e9dab9da7132ecff7c431166d9d
Marco Ricupero <marco.ricupero@gmail.com>
2018-05-13 02:13:29 +0200

    JSON parse and code parseInt error handling + tests

--- test/test.js
+++ test/test.js
@@ -9,56 +9,95 @@ chai.should();
 describe('mockettaro', ()=>{
     let testServer;
     before(function (done) {
-        server.use(`/test`, mockettaro.serve('/examples/mocks'));
+        server.use(`/test`, mockettaro.serve('/examples/mocks', {'errors': false}));
         testServer = server.listen(9999, done);
     })
-    describe('Cities Toulouse DELETE resource', ()=>{
-        it('Should return status 204 and an empty body', done => {
-            chai.request(server).delete('/test/cities/Toulouse')
-                .end((req, res)=>{
-                    res.status.should.be.equal(204);
-                    res.body.should.be.an('object').that.is.empty;
-                    done();
-                });
-        });
-    });
 
-    describe('Cities default PUT resource', () => {
-        it('Should return 202 and a body', done => {
-            chai.request(server).put('/test/cities/Rome')
-                .end((req, res) => {
-                    res.status.should.be.equal(202);
-                    res.body.should.be.an('object').that.have.property('status').that.is.equal("OK");
-                    done();
+    describe('REST server', () => {
+        describe('Cities resource', ()=>{
+            describe('Toulouse DELETE', () => {
+                it('Should return status 204 and an empty body', done => {
+                    chai.request(server).delete('/test/cities/Toulouse')
+                        .end((req, res) => {
+                            res.status.should.be.equal(204);
+                            res.body.should.be.an('object').that.is.empty;
+                            done();
+                        });
                 });
-        });
-    });
+            });
 
-    describe('Cities GET resource', ()=>{
-        it('Should return status 200 and an Array of cities on GET', done => {
-            chai.request(server).get('/test/cities')
-                .end((req, res) => {
-                    res.status.should.be.equal(200);
-                    res.body.should.be.an('array').that.is.not.empty;
-                    done();
+            describe('Default PUT', () => {
+                it('Should return 202 and a body', done => {
+                    chai.request(server).put('/test/cities/Rome')
+                        .end((req, res) => {
+                            res.status.should.be.equal(202);
+                            res.body.should.be.an('object').that.have.property('status').that.is.equal("OK");
+                            done();
+                        });
                 });
+            });
+
+            describe('GET', () => {
+                it('Should return status 200 and an Array of cities on GET', done => {
+                    chai.request(server).get('/test/cities')
+                        .end((req, res) => {
+                            res.status.should.be.equal(200);
+                            res.body.should.be.an('array').that.is.not.empty;
+                            done();
+                        });
+                });
+            });
+            describe('POST', () => {
+                it('Should return 201 and a body passing jsonschema validation', done => {
+                    chai.request(server).post('/test/cities').send({
+                            "name": "Naples"
+                        })
+                        .end((req, res) => {
+                            res.status.should.be.equal(201);
+                            res.body.should.be.an('object').that.have.property('status').that.is.equal("OK");
+                            done();
+                        });
+                });
+                it('Should return 400 against jsonschema validation', done => {
+                    chai.request(server).post('/test/cities')
+                        .end((req, res) => {
+                            res.status.should.be.equal(400);
+                            done();
+                        });
+                });
+            });
         });
-    });
-    describe('Cities POST resource', () => {
-        it('Should return 201 and a body passing jsonschema validation', done => {
-            chai.request(server).post('/test/cities').send({"name":"Naples"})
-                .end((req, res) => {
-                    res.status.should.be.equal(201);
-                    res.body.should.be.an('object').that.have.property('status').that.is.equal("OK");
-                    done();
+
+        describe('Error handling', () => {
+            describe('Json file read', () => {
+                it('Should return status 500 on GET', done => {
+                    chai.request(server).get('/test/errors/invalidJson')
+                        .end((req, res) => {
+                            res.status.should.be.equal(500);
+                            done();
+                        });
                 });
-        });
-        it('Should return 400 against jsonschema validation', done => {
-            chai.request(server).post('/test/cities')
-                .end((req, res) => {
-                    res.status.should.be.equal(400);
-                    done();
+            });
+
+            describe('Json Schema file read', () => {
+                it('Should return status 500 on POST', done => {
+                    chai.request(server).post('/test/errors/invalidSchema')
+                        .end((req, res) => {
+                            res.status.should.be.equal(500);
+                            done();
+                        });
                 });
+            });
+
+            describe('Code file read', () => {
+                it('Should return status 500 on GET', done => {
+                    chai.request(server).get('/test/errors/invalidCode')
+                        .end((req, res) => {
+                            res.status.should.be.equal(500);
+                            done();
+                        });
+                });
+            });
         });
     });
 
