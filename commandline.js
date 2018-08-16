fa90e28b0b0dc3f13253b913eea232a5bb0f2c25
Marco Ricupero <marco.ricupero@gmail.com>
2018-08-16 23:42:52 +0200

    commandline resource fix + version bump + sonar-scanner dependency

--- commandline.js
+++ commandline.js
@@ -7,10 +7,10 @@ require('colors');
 program
   .version(pkgjson.version, '-v, --version')
   .description(pkgjson.description)
-  .option('-p, --port <portNumber>', 'Serve on specified port', v=>parseInt(v), 8080)
-  .option('-r, --resource <resource>', 'Root resource to serve', /^[a-z0-9\_\-]*$/i, 'mockettaro')
-  .option('-d, --response-delay <responseDelay>', 'Response delay in ms', v=>parseInt(v), 0)
-  .option('-t, --cache-lifetime <cacheLifetime>', 'JSON cache lifetime', v=>parseInt(v), 3000)
+  .option('-p, --port <portNumber>', 'Serve on specified port', v => parseInt(v), 8080)
+  .option('-r, --resource <resource>', 'Root resource to serve', /^(?:\/?[a-z0-9\_\-]+)+$/i, 'mockettaro')
+  .option('-d, --response-delay <responseDelay>', 'Response delay in ms', v => parseInt(v), 0)
+  .option('-t, --cache-lifetime <cacheLifetime>', 'JSON cache lifetime', v => parseInt(v), 3000)
   .parse(process.argv);
 
 const mockettaro = require('./');
